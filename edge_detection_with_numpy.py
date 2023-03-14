70361569be72224c7cde0555c89953566c7de317
rashid <agp.rashid@gmail.com>
2023-03-14 14:37:40 +0100

    I solved the problem of numpy. I import the scipy

--- edge_detection_with_numpy.py
+++ edge_detection_with_numpy.py
@@ -1,14 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+from scipy.signal import convolve2d
 
-img = Image.open('../images/1.jpeg').convert('L')
 
+img = Image.open('./images/2.jpg').convert('L')
+#img_array = np.array(img)
+
+#img_array = img.size
 kernel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
 kernel_y = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
 
-img_x = np.abs(np.convolve(img, kernel_x, mode='same'))
-img_y = np.abs(np.convolve(img, kernel_y, mode='same'))
+img_x = np.abs(convolve2d(img, kernel_x, mode='same'))
+img_y = np.abs(convolve2d(img, kernel_y, mode='same'))
 edge = img_x + img_y
 
 
